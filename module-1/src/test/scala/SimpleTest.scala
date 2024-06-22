2df9e8058bea20554480b77cf2d8d0ce240f4261
Alex Kalinichev <akalinichev@natera.com>
2024-06-21 17:41:45 -0700

    module-1-01-logging

--- module-1/src/test/scala/SimpleTest.scala
+++ module-1/src/test/scala/SimpleTest.scala
@@ -1,7 +1,11 @@
+import com.typesafe.scalalogging.LazyLogging
+
 class SimpleTest
-  extends org.scalatest.funsuite.AnyFunSuite {
+  extends org.scalatest.funsuite.AnyFunSuite
+    with LazyLogging {
 
   test("Simple test") {
+    logger.info("Running Simple test")
     assert(1 == 1)
   }
 }
